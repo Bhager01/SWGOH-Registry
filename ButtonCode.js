0047a23fdba1ccc76e10f6c61787ad924865702e
Bhager01 <61237715+Bhager01@users.noreply.github.com>
2023-04-06 13:44:19 -0400

    Update ButtonCode.js

--- ButtonCode.js
+++ ButtonCode.js
@@ -1,28 +1,20 @@
 /*The below code needs to be placed where you handle button interactions.  If you do not have any buttons, you can place if(interaction.isButton()) in the code where
 you handle interactions.  The code below utilizes Discord interactions.  For any API calls, I recommend utilizing a retry with error catching mechanism.*/
 if (interaction.isButton()) {
-    if (interaction.customId === 'Global_Verify_Button') //ensure the below code only runs for SWGOH registery verify button
+    if (interaction.customId === 'Global_Verify_Button')
     {
         await interaction.deferUpdate({ephemeral: true })
         const embed = interaction.message.embeds[0]
         const allyCode = embed.title.replace(/\D/g, "")
-        const verificationTitleName = embed.fields.find(field => field.name === "Title").value;
-        const verificationPortraitName = embed.fields.find(field => field.name === "Portrait").value;
-
-        const comlinkPayload = {"payload": {"allyCode": String(allyCode)}, "enums": false}
+        const comlinkPayload = {'discordId':interaction.user.id, "method":"verification", "payload": {"allyCode": String(allyCode)}, "enums": false}
         const comlinkHeaders = {
             "Content-Type": "application/json",
-            "Authorization": "XXXXXXXXXXXX"
+            "Authorization": "XXXXXXXXXXXX" //replace with your API key
         }
-        
-        //This fetch is used to get the current player title and portrait
-        const comlinkResponse = await fetch("http://ec2-44-194-200-153.compute-1.amazonaws.com/api/comlink", {
-            method: "post",
-            headers: comlinkHeaders,
-            body: JSON.stringify(comlinkPayload),
-        })
-        
-        if(comlinkResponse == 'Error') //This line depends on your implementation.  I have a retry method that returns 'Error' if a non 2XX status code is returned.
+        //ComlinkFetch is my own function for doing API calls.  It has built in retry and error handling.  Use whatever method your currently have for API calls here.
+        const comlinkResponse = await ComlinkFetch("http://ec2-44-194-200-153.compute-1.amazonaws.com/api/comlink", comlinkHeaders, comlinkPayload)
+
+        if(comlinkResponse == 'Error')
         {
             await interaction.followUp({ content: 'Error when attempting to verify. Please click verify again.', ephemeral: true });
             console.error("Error contacting Comlink after verify was clicked.")
@@ -30,37 +22,15 @@ if (interaction.isButton()) {
         }
 
         const comlinkJSON = await comlinkResponse.json()
-        const verificationSuccess = (comlinkJSON.selectedPlayerTitle.name == verificationTitleName && comlinkJSON.selectedPlayerPortrait.name == verificationPortraitName)
-
-        if(verificationSuccess == true) //player has set their portrait and allycode to the required values for verification
-        {
-            const databasePayload = {'allyCode':String(allyCode), 'discordId':interaction.user.id, 'verified':'yes', 'endpoint':'upsert'}
-            const databaseHeaders = {
-                'api-key': 'XXXXXXXXX',
-                "Authorization": "XXXXXXXXX",
-                'Content-Type': 'application/json',
-            }            
-            
-            //This code updates the verified entry for user to yes
-            const databaseResponse =  await fetch("http://ec2-44-194-200-153.compute-1.amazonaws.com/api/database", {
-                method: "post",
-                headers: databaseHeaders,
-                body: JSON.stringify(databasePayload)
-            })
-            
-            if(databaseResponse == 'Error') //This line depends on your implementation.  I have a retry method that returns 'Error' if a non 2XX status code is returned.
-            {
-                await interaction.followUp({ content: 'Error when attempting to verify. Please click verify again.', ephemeral: true });
-                console.error("Error setting verified to yes after user clicked verify button")
-                return 0;
-            }
-
-            const row = new Discord.ActionRowBuilder().addComponents(new ButtonBuilder().setCustomId('Global_Verify_Button').setLabel('Verify').setStyle('Primary').setDisabled(true));
+        if(comlinkJSON == 'verified')
+        {   
+            const row = new Discord.ActionRowBuilder()
+                .addComponents(new ButtonBuilder().setCustomId('Global_Verify_Button').setLabel('Verify').setStyle('Primary').setDisabled(true));
             await interaction.editReply({ components: [row] })
             await interaction.followUp({ content: 'You have been sucessfully verified!', ephemeral: true });
         }
-
         else
-            await interaction.followUp({ content: 'Unable to verify. Please double check you have set the correct title & portrait & then click verify again.', ephemeral: true });
+            await interaction.followUp({ content: 'Unable to verify. Please double check you have set the correct title & portrait & then "
+            + "click verify again.', ephemeral: true });
     }
 }
