d78b61e34945105b0e91ef26baeb664a4a0385cd
Bhager01 <61237715+Bhager01@users.noreply.github.com>
2023-04-13 14:41:19 -0400

    Update FetchCode.js
    Update to document "primary" field being returned.

--- FetchCode.js
+++ FetchCode.js
@@ -1,5 +1,6 @@
 /*This code is used to lookup a user by allycode or Discord ID.  The 'user' field is an array of either Discord IDs or allycodes as strings (up to 50).  If no results
-are found, an empty array is returned.  Each item returned will include 3 fields:  allyCode, discordId, and verified./*
+are found, an empty array is returned.  Each item returned will include 3 fields:  allyCode, discordId, and verified.  The primary field will also be included if the
+user registered on or after 4-13-2023/*
 
 const data = {'user': ['561963325'],'endpoint': 'find'}
 
